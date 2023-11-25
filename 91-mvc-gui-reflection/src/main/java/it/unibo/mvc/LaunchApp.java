604367a5ad59815e3c098e53986e158b7a6584ac
Mattia Ronchi <mattia.ronchi5@studio.unibo.it>
2023-11-25 12:10:34 +0100

    reflection loading 91

--- 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,10 +1,12 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberConsoleView;
-import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
  * Application entry-point.
@@ -18,17 +20,22 @@ public final class LaunchApp {
      *
      * @param args ignored
      * @throws ClassNotFoundException if the fetches class does not exist
+     * @throws SecurityException
      * @throws NoSuchMethodException if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
      * @throws InstantiationException if the constructor throws exceptions
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberConsoleView());
+        for (String viewType: List.of("Console", "Swing")) {
+            for(int i = 0; i < 3; i++){
+                final var viewClass = Class.forName("it.unibo.mvc.view.DrawNumber" + viewType + "View");
+                final DrawNumberView view = (DrawNumberView) viewClass.getConstructor().newInstance();
+                app.addView(view);
+            }
+        }
     }
 }
