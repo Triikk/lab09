d6d6f6450e7d607373ee97e9bba2a274119973e8
Mattia Ronchi <mattia.ronchi5@studio.unibo.it>
2023-11-26 13:21:59 +0100

    implemented view and controller

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,42 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private String string = null;
+    private List<String> history = new ArrayList<>();
+
+    @Override
+    public void setString(String string) {
+        if(string.equals(null)){
+            throw new NullPointerException("The string provided was null");
+        }
+        this.string = string;
+    }
+
+    @Override
+    public String getString() {
+        return this.string;
+    }
+
+    @Override
+    public List<String> getPrintedStrings() {
+        return this.history;
+    }
+
+    @Override
+    public void print() {
+        if(this.string.equals(null)){
+            throw new IllegalStateException("The current string is unset");
+        }
+        System.out.println(this.string);
+        history.add(this.string);
+    }
 
 }
--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,16 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +19,53 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private final Controller controller = new SimpleController();
+
+    public SimpleGUI(){
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextField textField = new JTextField();
+        final JTextArea textArea = new JTextArea();
+        final JButton printButton = new JButton("Print");
+        final JButton showHistoryButton = new JButton("Show history");
+        canvas.add(textField, BorderLayout.NORTH);
+        canvas.add(textArea, BorderLayout.CENTER);
+        final JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.X_AXIS));
+        buttonsPanel.add(printButton);
+        buttonsPanel.add(showHistoryButton);
+        canvas.add(buttonsPanel, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        printButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                controller.setString(textField.getText());
+                controller.print();
+            }
+        });
+
+        showHistoryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                List<String> printedStrings = controller.getPrintedStrings();
+                String finalText = "";
+                for(String string : printedStrings){
+                    finalText += string + "\n";
+                }
+                textArea.setText(finalText);
+            }
+        });
+    }
+
+    public void display(){
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        final SimpleGUI gui = new SimpleGUI();
+        gui.display();
+    }
 
 }
