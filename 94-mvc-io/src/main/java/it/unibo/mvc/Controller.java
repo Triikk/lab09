5aa37ebc5fd3915b8bacae950cc331ada1823898
Mattia Ronchi <mattia.ronchi5@studio.unibo.it>
2023-11-26 12:57:53 +0100

    implemented interface Controller

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,33 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * This class models a simple controller responsible
+ * for I/O access.
  */
 public interface Controller {
 
+    /**
+     * Set the next string to print
+     * @param string the next string to print
+     * @throws NullPointerException if the string provided is null
+     */
+    void setString(String string);
+
+    /**
+     * Returns the next string to print
+     */
+    String getString();
+
+    /**
+     * @return a list of the printed strings
+    */
+    List<String> getPrintedStrings();
+
+    /**
+     * Prints the current string
+     * @throws IllegalStateException if the current string is unset
+     */
+    void print();
 }
