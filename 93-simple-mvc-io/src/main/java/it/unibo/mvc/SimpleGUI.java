60a08c3d8f86f6fd88c0fc12b453d9de929c43ab
Mattia Ronchi <mattia.ronchi5@studio.unibo.it>
2023-11-26 12:35:43 +0100

    implemented SimpleGUI

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,14 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +17,37 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+
+    public SimpleGUI(){
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextArea textArea = new JTextArea();
+        final JButton save = new JButton("Save");
+        canvas.add(textArea, BorderLayout.CENTER);
+        canvas.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                final Controller controller = new Controller();
+                try {
+                    controller.save(textArea.getText());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            } 
+        });
+    }
+
+    public void display(){
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        final SimpleGUI gui = new SimpleGUI();
+        gui.display();
+    }
 
 }
